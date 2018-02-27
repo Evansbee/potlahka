083a036691f89db06a434d9f62b54358ae703018
Evan Ackmann <evansbee@gmail.com>
2018-02-27 18:13:13 -0500

    added a camera and world module

---
+++ inc/world.hpp
@@ -0,0 +1,14 @@
+#prama once
+
+#include "config.hpp"
+#include "hittable.hpp"
+
+#include <vector>
+#include <memory>
+class world : public hittable
+{
+   public:
+      virtual bool hit(const ray& r, real tmin, real tmax, hit_recort &record) const override;
+   private:
+      std::vector<std::unique_ptr<hittable>> m_Entities;
+}
